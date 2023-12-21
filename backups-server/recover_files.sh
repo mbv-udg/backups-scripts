6e1579c3bfba6ba81d1a2262ab403dadbd7f51d4
mbv-udg <100859358+mbv-udg@users.noreply.github.com>
2023-12-21 18:20:54 +0000

    Update recover_files.sh

--- backups-server/recover_files.sh
+++ backups-server/recover_files.sh
@@ -2,27 +2,34 @@
 
 # PRE: files must exist.
 
-if [ $# -ne 4 ]; then
-    echo "Usage: ./recover_files.sh user backup path file"
+if [ $# -ne 5 ]; then
+    echo "Usage: ./recover_files.sh user backup path file isDir"
     exit 1
 fi
 
-readonly remote='192.168.126.189'
+readonly remote='192.168.18.131'
 readonly user=$1
 readonly backup=$2
 path=$3
 file=$4
 isDir=$5
 
+# Create folder if it doesn't exist
 ssh syncbackup@$remote mkdir -p $path
 
+# Different command depending it it's a directory or file
 if [ $isDir -eq 0 ]; then
-    error=$(rsync -aru --no-perms /home/xarxes/backups/$user/$backup/$path/$file syncbackup@$remote:/home/$user/files/$path/$file 2>&1> /dev/null)
+    error=$(rsync -ar --no-perms /home/xarxes/backups/$user/$backup/$path/$file syncbackup@$remote:/home/$user/files/$path/$file 2>&1> /dev/null)
 else
-    error=$(rsync -aru --no-perms /home/xarxes/backups/$user/$backup/$path/$file syncbackup@$remote:/home/$user/files/$path 2>&1> /dev/null)
+    error=$(rsync -ar --no-perms /home/xarxes/backups/$user/$backup/$path/$file syncbackup@$remote:/home/$user/files/$path 2>&1> /dev/null)
 fi
 
+# Don't show one specific error
 if [ $? -eq 23 ]; then
     lines=$(echo "$error" | wc -l)
-    [ $lines -eq 2 ] && [[ "$error" == *"failed to set times on \"/home/$user/files/.\""* ]] && exit 0
+    [ $lines -eq 2 ] && [[ "$error" == *"failed to set times on \"/home/$user/files/.\""* ]]
+elif [ $? -ne 0 ]; then
+    exit $?
 fi
+
+ssh syncbackup@$remote "sudo chown -R $user:sftp /home/$user/files/"
